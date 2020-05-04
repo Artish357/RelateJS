c88dd11523e86bed88fc4a192e99dd97a9a5e694
gregr <greg.weiqi@gmail.com>
2020-05-04 13:20:53 -0400

    Rename evalo-X* to eval-X*o

--- evalo-tests.rkt
+++ evalo-tests.rkt
@@ -1,8 +1,8 @@
 #lang racket
 (require "evalo.rkt" "faster-miniKanren/mk.rkt" "js-structures.rkt")
 
-(define (evalo-ns exp val)
-  (fresh (next-address^ store) (evalo-env exp `() val `() store `() next-address^)))
+(define (evalo/ns exp val)
+  (fresh (next-address^ store) (eval-envo exp `() val `() store `() next-address^)))
 
 (module+ test
   (require rackunit)
@@ -14,112 +14,112 @@
   (let ([1to6zip (list (cons (jstr "1") (jnum 2)) (cons (jstr "3") (jnum 4)) (cons (jstr "5") (jnum 6)))]
         [1to3 (map jnum `(1 2 3))])
     (test= "Get #1"
-           (run* (res) (fresh (store) (evalo-ns (jget (jobj 1to6zip) (jstr "1")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jget (jobj 1to6zip) (jstr "1")) res)))
            `(,(jnum 2)))
     (test= "Get #2"
-           (run* (res) (fresh (store) (evalo-ns (jget (jobj 1to6zip) (jstr "3")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jget (jobj 1to6zip) (jstr "3")) res)))
            `(,(jnum 4)))
     (test= "Get #3"
-           (run* (res) (fresh (store) (evalo-ns (jget (jobj 1to6zip) (jstr "5")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jget (jobj 1to6zip) (jstr "5")) res)))
            `(,(jnum 6)))
     (test= "Get, empty"
-           (run* (res) (fresh (store) (evalo-ns (jget (jobj `()) (jstr "1")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jget (jobj `()) (jstr "1")) res)))
            `(,(jundef)))
     (test= "Get, not found"
-           (run* (res) (fresh (store) (evalo-ns (jget (jobj 1to6zip) (jstr "4")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jget (jobj 1to6zip) (jstr "4")) res)))
            `(,(jundef)))
     (test= "Update #1"
-           (run* (res) (fresh (store) (evalo-ns (jset (jobj 1to6zip) (jstr "1") (jnum 100)) res)))
+           (run* (res) (fresh (store) (evalo/ns (jset (jobj 1to6zip) (jstr "1") (jnum 100)) res)))
            `(,(jobj `((,(jstr "1") . ,(jnum 100)) . ,(cdr 1to6zip)))))
     (test= "Update #2"
-           (run* (res) (fresh (store) (evalo-ns (jset (jobj 1to6zip) (jstr "3") (jnum 100)) res)))
+           (run* (res) (fresh (store) (evalo/ns (jset (jobj 1to6zip) (jstr "3") (jnum 100)) res)))
            `(,(jobj `((,(jstr "1") . ,(jnum 2)) (,(jstr "3") . ,(jnum 100)) (,(jstr "5") . ,(jnum 6))))))
     (test= "Update #3"
-           (run* (res) (fresh (store) (evalo-ns (jset (jobj 1to6zip) (jstr "5") (jnum 100)) res)))
+           (run* (res) (fresh (store) (evalo/ns (jset (jobj 1to6zip) (jstr "5") (jnum 100)) res)))
            `(,(jobj `((,(jstr "1") . ,(jnum 2)) (,(jstr "3") . ,(jnum 4)) (,(jstr "5") . ,(jnum 100))))))
     (test= "Create"
-           (run* (res) (fresh (store) (evalo-ns (jset (jobj 1to6zip) (jstr "0") (jnum 100)) res)))
+           (run* (res) (fresh (store) (evalo/ns (jset (jobj 1to6zip) (jstr "0") (jnum 100)) res)))
            '((object
               (((string ((1 0 0 0 1 1))) number (0 1))
                ((string ((1 1 0 0 1 1))) number (0 0 1))
                ((string ((1 0 1 0 1 1))) number (0 1 1))
                ((string ((0 0 0 0 1 1))) number (0 0 1 0 0 1 1))))))
     (test= "Create, empty object"
-           (run* (res) (fresh (store) (evalo-ns (jset (jobj `()) (jstr "0") (jnum 100)) res)))
+           (run* (res) (fresh (store) (evalo/ns (jset (jobj `()) (jstr "0") (jnum 100)) res)))
            `(,(jobj `((,(jstr "0") . ,(jnum 100))))))
     (test= "Delete #1"
-           (run* (res) (fresh (store) (evalo-ns (jdel (jobj 1to6zip) (jstr "1")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jdel (jobj 1to6zip) (jstr "1")) res)))
            `(,(jobj (cdr 1to6zip))))
     (test= "Delete #2"
-           (run* (res) (fresh (store) (evalo-ns (jdel (jobj 1to6zip) (jstr "3")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jdel (jobj 1to6zip) (jstr "3")) res)))
            `(,(jobj `((,(jstr "1") . ,(jnum 2)) (,(jstr "5") . ,(jnum 6))))))
     (test= "Delete #3"
-           (run* (res) (fresh (store) (evalo-ns (jdel (jobj 1to6zip) (jstr "5")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jdel (jobj 1to6zip) (jstr "5")) res)))
            `(,(jobj `((,(jstr "1") . ,(jnum 2)) (,(jstr "3") . ,(jnum 4))))))
     (test= "Delete, not found"
-           (run* (res) (fresh (store) (evalo-ns (jdel (jobj 1to6zip) (jstr "0")) res)))
+           (run* (res) (fresh (store) (evalo/ns (jdel (jobj 1to6zip) (jstr "0")) res)))
            `(,(jobj 1to6zip)))
     (test= "Variable reference #1"
-           (run* (res) (fresh (store) (evalo-env (jvar `x) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
+           (run* (res) (fresh (store) (eval-envo (jvar `x) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
            `(1))
     (test= "Variable reference #2"
-           (run* (res) (fresh (store) (evalo-env (jvar `y) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
+           (run* (res) (fresh (store) (eval-envo (jvar `y) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
            `(2))
     (test= "Variable reference #3"
-           (run* (res) (fresh (store) (evalo-env (jvar `z) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
+           (run* (res) (fresh (store) (eval-envo (jvar `z) `((x . 1) (y . 2) (z . 3)) res `() `() `() `())))
            `(3))
     (test= "Function application, no parameters"
-           (run* (res) (fresh (store) (evalo-ns (japp (jfun `() (jdelta `+ `(,(jnum 1) ,(jnum 5)))) `()) res)))
+           (run* (res) (fresh (store) (evalo/ns (japp (jfun `() (jdelta `+ `(,(jnum 1) ,(jnum 5)))) `()) res)))
            `(,(jnum 6)))
     (test= "Function application, parameter #1"
-           (run* (res) (fresh (store) (evalo-ns (japp (jfun `(x y z) (jvar `x)) 1to3) res)))
+           (run* (res) (fresh (store) (evalo/ns (japp (jfun `(x y z) (jvar `x)) 1to3) res)))
            `(,(jnum 1)))
     (test= "Function application, parameter #2"
-           (run* (res) (fresh (store) (evalo-ns (japp (jfun `(x y z) (jvar `y)) 1to3) res)))
+           (run* (res) (fresh (store) (evalo/ns (japp (jfun `(x y z) (jvar `y)) 1to3) res)))
            `(,(jnum 2)))
     (test= "Function application, parameter #3"
-           (run* (res) (fresh (store) (evalo-ns (japp (jfun `(x y z) (jvar `z)) 1to3) res)))
+           (run* (res) (fresh (store) (evalo/ns (japp (jfun `(x y z) (jvar `z)) 1to3) res)))
            `(,(jnum 3)))
     (test= "Allocation"
-           (run* (val store next-address) (evalo-env (jall (jnum 100)) `() val `() store `() next-address))
+           (run* (val store next-address) (eval-envo (jall (jnum 100)) `() val `() store `() next-address))
            `(( ,(jref `()) (,(jnum 100))  (()) )))
     (test= "Dereference"
-           (run* (val store next-address) (evalo-env (jderef (jref `())) `() val `(,(jnum 123)) store `(()) next-address))
+           (run* (val store next-address) (eval-envo (jderef (jref `())) `() val `(,(jnum 123)) store `(()) next-address))
            `((,(jnum 123) (,(jnum 123))  (()) )))
     (test= "Assignment"
-           (run* (val store next-address) (evalo-env (jassign (jref `()) (jnum 5)) `() val `(,(jnum 0)) store `(()) next-address))
+           (run* (val store next-address) (eval-envo (jassign (jref `()) (jnum 5)) `() val `(,(jnum 0)) store `(()) next-address))
            `((,(jnum 5) (,(jnum 5))  (()) )))
     (test= "Combined memory functions"
-           (run* (val store next-address) (evalo-env (jassign (jall (jnum 66)) (jderef (jref `()))) `() val `(,(jnum 33)) store `(()) next-address))
+           (run* (val store next-address) (eval-envo (jassign (jall (jnum 66)) (jderef (jref `()))) `() val `(,(jnum 33)) store `(()) next-address))
            `((,(jnum 33) (,(jnum 33) ,(jnum 33))  ((())) )))
     (let ((testfunc (jfun `(x)
                           (jbeg
                            (jall (jvar `x))
                            (jif (jderef (jref `())) (jnum 1) (jnum 0))))))
       (test= "Control structures #1"
-             (run* (val) (evalo-ns (japp testfunc `(,(jbool #t))) val))
+             (run* (val) (evalo/ns (japp testfunc `(,(jbool #t))) val))
              `(,(jnum 1)))
       (test= "Control structures #2"
-             (run* (val) (evalo-ns (japp testfunc `(,(jbool #f))) val))
+             (run* (val) (evalo/ns (japp testfunc `(,(jbool #f))) val))
              `(,(jnum 0)))
       )
     (let ([breakval (jbrk `error `e)])
       (test= "Basic break"
-             (run* (val) (evalo-ns breakval val))
+             (run* (val) (evalo/ns breakval val))
              `(,breakval))
       (test= "Catch, no break"
-             (run* (val) (evalo-ns (jcatch `error (jnum 0) `err-var (jvar `err-var)) val))
+             (run* (val) (evalo/ns (jcatch `error (jnum 0) `err-var (jvar `err-var)) val))
              `(,(jnum 0)))
       (test= "Basic catch"
-             (run* (val) (evalo-ns (jcatch `error breakval `err-var (jvar `err-var)) val))
+             (run* (val) (evalo/ns (jcatch `error breakval `err-var (jvar `err-var)) val))
              `(e))
       (test= "Basic catch, wrong label"
-             (run* (val) (evalo-ns (jcatch `loop-break breakval `err-var (jvar `err-var)) val))
+             (run* (val) (evalo/ns (jcatch `loop-break breakval `err-var (jvar `err-var)) val))
              `(,breakval))
       )
     )
   (test= "typeof"
-         (map (lambda (x) (run* (val) (evalo-ns (jdelta `typeof `(,x)) val)))
+         (map (lambda (x) (run* (val) (evalo/ns (jdelta `typeof `(,x)) val)))
               (list
                (jundef)
                (jnul)
@@ -131,42 +131,42 @@
                ))
          (map (lambda (x) `(,(jstr x))) (list "undefined" "object" "number" "string" "boolean" "object" "function")))
   (test= "+"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `+ `(,(jnum 20) ,(jnum 10))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `+ `(,(jnum 20) ,(jnum 10))) res)))
          `(,(jnum 30)))
   (test= "-"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `- `(,(jnum 20) ,(jnum 10))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `- `(,(jnum 20) ,(jnum 10))) res)))
          `(,(jnum 10)))
   (test= "*"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `* `(,(jnum 20) ,(jnum 10))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `* `(,(jnum 20) ,(jnum 10))) res)))
          `(,(jnum 200)))
   (test= "/"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `/ `(,(jnum 20) ,(jnum 10))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `/ `(,(jnum 20) ,(jnum 10))) res)))
          `(,(jnum 2)))
   (test= "< #1"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `< `(,(jnum 20) ,(jnum 10))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `< `(,(jnum 20) ,(jnum 10))) res)))
          `(,(jbool #f)))
   (test= "< #2"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `< `(,(jnum 10) ,(jnum 20))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `< `(,(jnum 10) ,(jnum 20))) res)))
          `(,(jbool #t)))
   (test= "string-+"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-+ `(,(jstr "Hello") ,(jstr "World"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-+ `(,(jstr "Hello") ,(jstr "World"))) res)))
          `(,(jstr "HelloWorld")))
   (test= "string-< #1"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-< `(,(jstr "Hell") ,(jstr "Hello"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-< `(,(jstr "Hell") ,(jstr "Hello"))) res)))
          `(,(jbool #t)))
   (test= "string-< #2"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-< `(,(jstr "Helloo") ,(jstr "Hello"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-< `(,(jstr "Helloo") ,(jstr "Hello"))) res)))
          `(,(jbool #f)))
   (test= "string-< #3"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-< `(,(jstr "Helal") ,(jstr "Helbl"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-< `(,(jstr "Helal") ,(jstr "Helbl"))) res)))
          `(,(jbool #t)))
   (test= "string-< #4"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-< `(,(jstr "Helbl") ,(jstr "Helal"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-< `(,(jstr "Helbl") ,(jstr "Helal"))) res)))
          `(,(jbool #f)))
   (test= "string-< #5"
-         (run* (res) (fresh (store) (evalo-ns (jdelta `string-< `(,(jstr "H") ,(jstr "H"))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `string-< `(,(jstr "H") ,(jstr "H"))) res)))
          `(,(jbool #f)))
   (test= "Combined delta test" ;; chr(ord('a')+2) -> 'c'
-         (run* (res) (fresh (store) (evalo-ns (jdelta `nat->char `(,(jdelta `+ `(,(jdelta `char->nat `(,(jstr "a"))) ,(jnum 2))))) res)))
+         (run* (res) (fresh (store) (evalo/ns (jdelta `nat->char `(,(jdelta `+ `(,(jdelta `char->nat `(,(jstr "a"))) ,(jnum 2))))) res)))
          `(,(jstr "c")))
   )
--- evalo.rkt
+++ evalo.rkt
@@ -1,11 +1,11 @@
 #lang racket
 (require "faster-miniKanren/mk.rkt" "js-structures.rkt" "faster-miniKanren/numbers.rkt" "helpers.rkt")
-(provide evalo evalo-env)
+(provide evalo eval-envo)
 
 (define (evalo exp val store)
-  (fresh (next-address^) (evalo-env exp `() val `() store `() next-address^)))
+  (fresh (next-address^) (eval-envo exp `() val `() store `() next-address^)))
 
-(define (evalo-env exp env value store store~ next-address next-address~)
+(define (eval-envo exp env value store store~ next-address next-address~)
   (conde ((== exp value)
           (== store store~)
           (== next-address next-address~)
@@ -14,11 +14,11 @@
           )
          ((fresh (k v v^ exp2 env2 store^ next-address^) ;; Let
                  (== exp (jlet k v exp2))
-                 (evalo/propagation evalo-env v env v^ value
+                 (evalo/propagation eval-envo v env v^ value
                                     store store^ store~
                                     next-address next-address^ next-address~
                                     (== env2 `((,k . ,v^) . ,env))
-                                    (evalo-env exp2 env2 value store^ store~ next-address^ next-address~))))
+                                    (eval-envo exp2 env2 value store^ store~ next-address^ next-address~))))
          ((fresh (var) ;; Look up a variable
                  (== exp (jvar var))
                  (== store store~)
@@ -32,15 +32,15 @@
                  (== next-address next-address~)))
          ((fresh (func params args args-eval body cenv cenv^ zipped store^ next-address^) ;; Function application
                  (== exp (japp func args))
-                 (evalo/propagation evalo-env-list `(,func . ,args) env (value-list `(,(jclo params body cenv) . ,args-eval)) value
+                 (evalo/propagation eval-env-listo `(,func . ,args) env (value-list `(,(jclo params body cenv) . ,args-eval)) value
                                     store store^ store~
                                     next-address next-address^ next-address~
                                     (zipo zipped params args-eval)
                                     (appendo zipped cenv cenv^)
-                                    (evalo-env body cenv^ value store^ store~ next-address^ next-address~))))
+                                    (eval-envo body cenv^ value store^ store~ next-address^ next-address~))))
          ((fresh (obj-exp bindings key key^) ;; Get
                  (== exp (jget obj-exp key))
-                 (evalo/propagation evalo-env-list `(,key ,obj-exp) env (value-list `(,key^ ,(jobj bindings))) value
+                 (evalo/propagation eval-env-listo `(,key ,obj-exp) env (value-list `(,key^ ,(jobj bindings))) value
                                     store store~ store~
                                     next-address next-address~ next-address~
                                     (typeofo key^ (jstr "string") store~)
@@ -50,7 +50,7 @@
                                             )))))
          ((fresh (obj-exp bindings bindings^ key key^ val val^ v) ;; Create/update field
                  (== exp (jset obj-exp key val))
-                 (evalo/propagation evalo-env-list `(,obj-exp ,key ,val) env (value-list `(,(jobj bindings) ,key^ ,val^)) value
+                 (evalo/propagation eval-env-listo `(,obj-exp ,key ,val) env (value-list `(,(jobj bindings) ,key^ ,val^)) value
                                     store store~ store~
                                     next-address next-address~ next-address~
                                     (== value (jobj bindings^))
@@ -59,17 +59,17 @@
                                     (updato bindings key^ val^ bindings^))))
          ((fresh (obj-exp bindings bindings^ key key^ store^ next-address^) ;; Delete field
                  (== exp (jdel obj-exp key))
-                 (evalo/propagation evalo-env-list `(,obj-exp ,key) env (value-list `(,(jobj bindings) ,key^)) value
+                 (evalo/propagation eval-env-listo `(,obj-exp ,key) env (value-list `(,(jobj bindings) ,key^)) value
                                     store store~ store~
                                     next-address next-address~ next-address~
                                     (== value (jobj bindings^))
                                     (typeofo key^ (jstr "string") store~)
                                     (deleto bindings key^ bindings^)
-                                    (evalo-env key env key^ store^ store~ next-address^ next-address~)
+                                    (eval-envo key env key^ store^ store~ next-address^ next-address~)
                                     )))
          ((fresh (ref ref^ next-address^ store^) ;; Allocate memory
                  (== exp (jall ref))
-                 (evalo/propagation evalo-env ref env ref^ value
+                 (evalo/propagation eval-envo ref env ref^ value
                                     store store^ store~
                                     next-address next-address^ next-address~
                                     (== value (jref next-address))
@@ -78,13 +78,13 @@
                                     )))
          ((fresh (addr-exp addr) ;; Fetch from memory
                  (== exp (jderef addr-exp))
-                 (evalo/propagation evalo-env addr-exp env (jref addr) value
+                 (evalo/propagation eval-envo addr-exp env (jref addr) value
                                     store store~ store~
                                     next-address next-address~ next-address~
                                     (indexo store~ addr value))))
          ((fresh (var val addr val^ store^) ;; Assign to memory address
                  (== exp (jassign var val))
-                 (evalo/propagation evalo-env-list `(,var ,val) env (value-list `(,(jref addr) ,val^)) value
+                 (evalo/propagation eval-env-listo `(,var ,val) env (value-list `(,(jref addr) ,val^)) value
                                     store store^ store~
                                     next-address next-address~ next-address~
                                     (== value val^)
@@ -92,30 +92,30 @@
                                     )))
          ((fresh (first second dummy) ;; Begin-discard
                  (== exp (jbeg first second))
-                 (evalo/propagation evalo-env-list `(,first ,second) env (value-list `(,dummy ,value)) value
+                 (evalo/propagation eval-env-listo `(,first ,second) env (value-list `(,dummy ,value)) value
                                     store store~ store~ next-address next-address~ next-address~)))
          ((fresh (cond cond^ then else store^ next-address^) ;; If statements
                  (== exp (jif cond then else))
-                 (evalo/propagation evalo-env cond env cond^ value
+                 (evalo/propagation eval-envo cond env cond^ value
                                     store store^ store~
                                     next-address next-address^ next-address~
-                                    (conde ((== cond^ (jbool #f)) (evalo-env else env value store^ store~ next-address^ next-address~))
-                                           ((== cond^ (jbool #t)) (evalo-env then env value store^ store~ next-address^ next-address~))))))
+                                    (conde ((== cond^ (jbool #f)) (eval-envo else env value store^ store~ next-address^ next-address~))
+                                           ((== cond^ (jbool #t)) (eval-envo then env value store^ store~ next-address^ next-address~))))))
          ((fresh (cond body store^ next-address^ cond^) ;; While
                  (== exp (jwhile cond body))
-                 (evalo/propagation evalo-env cond env cond^ value
+                 (evalo/propagation eval-envo cond env cond^ value
                                     store store^ store~
                                     next-address next-address^ next-address~
                                     (conde ((== cond^ (jbool #f)) (== value (jundef)) (== store^ store~) (== next-address^ next-address~))
-                                           ((== cond^ (jbool #t)) (evalo-env (jbeg body (jwhile cond body)) env value store^ store~ next-address^ next-address~))))))
+                                           ((== cond^ (jbool #t)) (eval-envo (jbeg body (jwhile cond body)) env value store^ store~ next-address^ next-address~))))))
          ((fresh (try-exp finally-exp try-value store^ next-address^) ;; Finally
                  (== exp (jfin try-exp finally-exp))
-                 (evalo-env try-exp env try-value store store^ next-address next-address^)
-                 (evalo/propagation evalo-env finally-exp env (value-list value) value
+                 (eval-envo try-exp env try-value store store^ next-address next-address^)
+                 (evalo/propagation eval-envo finally-exp env (value-list value) value
                                     store^ store~ store~ next-address next-address~ next-address~)))
          ((fresh (label label^ try-exp catch-var catch-exp try-value store^ next-address^ env^ break-value first rest) ;; Catch
                  (== exp (jcatch label try-exp catch-var catch-exp))
-                 (evalo-env try-exp env try-value store store^ next-address next-address^)
+                 (eval-envo try-exp env try-value store store^ next-address next-address^)
                  (conde ((== try-value (jbrk label^ break-value)) ;; Break does not match label
                          (== `(,value ,store~ ,next-address~) `(,try-value ,store^ ,next-address^))
                          (=/= label^ label)
@@ -126,19 +126,19 @@
                          )
                         ((== try-value (jbrk label break-value)) ;; Exception was caught
                          (appendo env `((,catch-var . ,break-value)) env^)
-                         (evalo-env catch-exp env^ value store^ store~ next-address^ next-address~)))))
+                         (eval-envo catch-exp env^ value store^ store~ next-address^ next-address~)))))
          ((jdeltao env exp value store store~ next-address next-address~))
          ((fresh (label val val^) ;; Throw
                  (== exp (jthrow label val))
                  (== value (jbrk label val^))
-                 (evalo-env val env val^ store store~ next-address next-address~)
+                 (eval-envo val env val^ store store~ next-address next-address~)
                  ))
          ))
 
 (define (jdeltao env exp value store store~ next-address next-address~)
   (fresh (func vals op1 op2 v1 v2 vals^ value^ rem)
          (== exp (jdelta func vals))
-         (evalo/propagation evalo-env-list vals env (value-list vals^) value
+         (evalo/propagation eval-env-listo vals env (value-list vals^) value
                             store store~ store~
                             next-address next-address~ next-address~
                             (conde ((== `(,func (,op1)) `(typeof ,vals^)) ;; Typeof
@@ -204,12 +204,12 @@
                 ((== s1 `(,x . ,x^)) (== s2 `(,y . ,y^)) (== value (jbool #t)) (=/= x y) (<o x y))
                 ((== s1 `(,x . ,x^)) (== s2 `(,y . ,y^)) (== value (jbool #f)) (=/= x y) (<=o y x)))))
 
-(define-syntax-rule (evalo/propagation evalo-func exp env val value
+(define-syntax-rule (evalo/propagation eval-funco exp env val value
                                        store store^ store~
                                        next-address next-address^ next-address~
                                        cont ...)
   (fresh (intermediate-val label bval tag rest)
-         (evalo-func exp env intermediate-val store store^ next-address next-address^)
+         (eval-funco exp env intermediate-val store store^ next-address next-address^)
          (conde ((== (jbrk label bval) intermediate-val)
                  (== `(,value ,store~ ,next-address~) `(,intermediate-val ,store^ ,next-address^)))
                 ((== `(,tag . ,rest) intermediate-val)
@@ -217,14 +217,14 @@
                  (=/= tag `break)
                  cont ...))))
 
-(define (evalo-env-list elist env vlist store store~ next-address next-address~)
+(define (eval-env-listo elist env vlist store store~ next-address next-address~)
   (conde ((== elist `()) (== vlist (value-list `())) (== store store~) (== next-address next-address~))
          ((fresh (exp exp-rest val value-rest value-rest^ store^ next-address^)
                  (== elist `(,exp . ,exp-rest))
-                 (evalo/propagation evalo-env exp env val vlist
+                 (evalo/propagation eval-envo exp env val vlist
                                     store store^ store~
                                     next-address next-address^ next-address~
-                                    (evalo/propagation evalo-env-list exp-rest env value-rest vlist
+                                    (evalo/propagation eval-env-listo exp-rest env value-rest vlist
                                                        store^ store~ store~
                                                        next-address^ next-address~ next-address~
                                                        (== value-rest (value-list value-rest^))
