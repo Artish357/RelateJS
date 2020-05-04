7b51f975534c2262599935807943cf8c7cab76f0
gregr <greg.weiqi@gmail.com>
2020-05-04 13:33:58 -0400

    Rename pull to hoist

--- parseo-tests.rkt
+++ parseo-tests.rkt
@@ -8,14 +8,14 @@
                  (begin (printf "Running: ~s\n" name)
                         (time expr))
                  output))
-  (test= "Pull vars, all undefined"
-         (run* (pairs) (pull-varo `(begin (var a b c) (if () (var q) (var y))) pairs))
+  (test= "Hoist vars, all undefined"
+         (run* (pairs) (hoist-varo `(begin (var a b c) (if () (var q) (var y))) pairs))
          '((a b c q y)))
-  (test= "Pull vars, some assigned"
-         (run* (pairs) (pull-varo `(begin (var (a 1) b (c 2)) (if () (var (q 3)) (var y))) pairs))
+  (test= "Hoist vars, some assigned"
+         (run* (pairs) (hoist-varo `(begin (var (a 1) b (c 2)) (if () (var (q 3)) (var y))) pairs))
          '((a b c q y)))
-  (test= "Pull vars, nested functions"
-         (run* (pairs) (pull-varo `(begin (var (a 1) b (c (function (x) (var should-not-pop-up)))) (if () (var (q 3)) (var y))) pairs))
+  (test= "Hoist vars, nested functions"
+         (run* (pairs) (hoist-varo `(begin (var (a 1) b (c (function (x) (var should-not-pop-up)))) (if () (var (q 3)) (var y))) pairs))
          '((a b c q y)))
   (test= "Object creation"
          (run* (r) (fresh (code i store)
