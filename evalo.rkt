29bb84a292aa58a0e43663bf2bde8cc1bab1151f
gregr <greg.weiqi@gmail.com>
2020-05-07 11:11:38 -0400

    Evaluate field values in LambdaJS object literals

--- evalo.rkt
+++ evalo.rkt
@@ -17,7 +17,6 @@
      (== store store~)
      (== next-address next-address~)
      (conde ((fresh (payload) (== expr (jrawnum payload))))
-            ((fresh (binds) (== expr (jobj binds))))
             ((fresh (b) (== expr (jbool b))))
             ((fresh (payload) (== expr (jrawstr payload))))
             ((== expr (jundef)))
@@ -61,6 +60,17 @@
          (appendo param-arg-bindings cenv cenv^)
          (eval-envo body cenv^ value store^ store~
                     next-address^ next-address~))))
+    ;; Object literals (Section 2.2.4)
+    ((fresh (binding-exprs field-names rhs-exprs value^
+             binding-values rhs-values)
+       (== expr `(object ,binding-exprs))
+       (zipo field-names rhs-exprs binding-exprs)
+       (eval-env-listo rhs-exprs env value^ store store~
+                       next-address next-address~)
+       (effect-propagateo value^ value store~ store~ next-address~ next-address~
+         (== value^ (value-list rhs-values))
+         (== value `(object ,binding-values))
+         (zipo field-names rhs-values binding-values))))
     ;; Object field retrieval (2.2.4)
     ((fresh (obj-expr obj-bindings key-expr key-val value^)
        (== expr (jget obj-expr key-expr))
